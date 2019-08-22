78f62e0c54e4d46fe875fd57b1cb066ef91d5ca8
djhylander <41591645+djhylander@users.noreply.github.com>
2019-08-22 15:02:32 -0700

    Update SGD randomly selected pairings.py

--- SGD randomly selected pairings.py
+++ SGD randomly selected pairings.py
@@ -2,17 +2,17 @@
 """
 Created on Thu Apr 17 20:29:58 2019
 
-@author: dhyla
-
 Calculates the most ideal similarity matrix using randomly selected unique pairings of a list of peptides until below a certain error
+
+*First iteration of the  code for this project9
+@author: dhyla
 """
 
 import pandas as pd
 import numpy as np
 from collections import OrderedDict
-#import time
 
-# the avergae time (1000 attempts) taken to calc all unique onehot pairings for 10 peptides: 0.055 seconds
+# calculates one-hot given a peptide
 def oneHotter(peptide):
     aminoAcids = list(peptide)  # below: initialize ordered dictionary to hold empty lists of amino acid length
     aaDict = OrderedDict()
@@ -35,29 +35,31 @@ def findPSS():
 def findGradient():  # Error = |TrueAffinity - pi *pj * S|^2  
     gradient = -2 * (actualAffinity - findPSS()) * np.dot(oneHot1, oneHot2).sum()  # Derivative = -2 * |TrueAffinity - pi *pj * S| * (pi * pj)   
     return gradient 
-    
-def minMatrix():  # minimizes the similarity matrix via gradient descent 
+
+# minimizes the similarity matrix via gradient descent 
+def minMatrix():  
     alpha = .01  # weight of gradient
     for i in range(100):
         gradient = findGradient()
-        if (abs(gradient) > .01):  # terminate if iterations > 10000 or change in matrix < .0001
+        if (abs(gradient) > .001):  # terminate if iterations > 1000 or change in matrix < .001
             change = alpha * gradient
             global simMatrix 
             simMatrix -= change # new similarity matrix
             alpha *= .98  # to ensure we don't skip over minimum indefinitely, don't know what is right momentum
         else:
             break
-    
+
+#Basic optimization of a similarity matrix            
 simMatrix = np.random.rand(20, 20)
 np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
 print("Inital random sim matrix: \n\n", simMatrix)
-data = pd.read_csv("peptideTest10.csv", usecols = ["AA_seq"])
+data = pd.read_csv("cleaned_set1.csv", usecols = ["AA_seq"])
 pepCol = list(data["AA_seq"])  # create list using the column of peptides
 numCol = []
 for i in range(len(pepCol)):  # create a list with the indexes of the peptides
     numCol.append(i)
 pairs = []
-for n in range(1000):  # used for "infinite" loop until error is low enough 
+for n in range(len(data)): 
     i = np.random.randint(0, len(pepCol))
     j = np.random.randint(0, len(pepCol))
     if (i != j):
